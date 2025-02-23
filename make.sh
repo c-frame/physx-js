fee613be6ba3abf21a030f2906038eea95ec9e0c
Vincent Fretin <vincent.fretin@gmail.com>
2025-02-23 21:56:20 +0100

    fix make.sh script to create dist folder

--- make.sh
+++ make.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 
 cd PhysX/physx/compiler/emscripten-release/
 make
-mkdir -p /src/lib
+mkdir -p /src/dist
 cp /src/PhysX/physx/bin/emscripten/release/physx.release.js /src/dist/physx.release.js
 cp /src/PhysX/physx/bin/emscripten/release/physx.release.wasm /src/dist/physx.release.wasm
