666dedea77c47cae14e627af84c382d76d7d4817
Vincent Fretin <vincent.fretin@gmail.com>
2025-02-24 11:41:42 +0100

    add support for running 'npm run generate' with official emscripten/emsdk docker image

--- generate.sh
+++ generate.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 cd ./PhysX/physx
+# If we're using the official emscripten/emsdk docker image instead of the deprecated trzeci/emscripten image,
+# symlink python3 to python and define the EMSCRIPTEN environment variable used in buildtools/cmake_generate_projects.py
+# called by ./generate_projects.sh
+if [ ! -f /usr/bin/python ]; then
+  ln -s /usr/bin/python3 /usr/bin/python
+  export EMSCRIPTEN=/emsdk/upstream/emscripten
+fi
 ./generate_projects.sh emscripten
